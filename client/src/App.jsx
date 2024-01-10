d66362a5459dfeedfcec83bdfdba9fa564c26a09
RAHUL CHOUDHARY <rc725383@gmail.com>
2024-01-10 17:18:15 +0530

    responcive footer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Container/Header'
+import Footer from './Container/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Footer/>
      
     </>
   )
